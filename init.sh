a29382aa8b8a65d2365e1ecfeac218330dc85b22
ahmedashrafdev <ahmad.ashraf.dev@gmail.com>
2021-06-15 17:54:35 +0200

    remove home computer bug cause from gitignor

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ sudo composer update
 
 sudo chmod -R 775 $(pwd)
 sudo chown -R www-data:www-data $(pwd)
-sudo mysql -u root --password=asd@asd@ esolveeg_systemapi < $(pwd)"/db/esolveeg_systemapi.sql"
+sudo mysql -u root --password=asd@asd@ test2 < $(pwd)"/db/esolveeg_systemapi.sql"
